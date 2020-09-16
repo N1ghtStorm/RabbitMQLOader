efd5449b670fb68ccba5ea0d6b059f3ba8382e19
N1ghtStorm <bushuevdv@mail.ru>
2020-09-16 16:25:06 +0300

    добавил обработку ошибок

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,49 @@ use std::env;
 
 
 fn main() {
+    const ARG_COUNT: usize = 6;
+    const HOST_ARG: usize = 1;
+    const USER_ARG: usize = 2;
+    const PASS_ARG: usize = 3;
+    const QUEUE_ARG: usize = 4;
+    const LENGTH_ARG: usize = 5;
+    const PUB_CONF_ARG: usize = 6;
+    const HELP_PHRASE: &str = "usage: host user pass queue_name message_len(usize) (publisher_confirms | true | false | хоть что)";
+
     let args: Vec<String> = env::args().collect();
     let mut publisher_confirms = false;
 
     if args.len() == 1 {
-        println!("usage: host user pass queue_name message_len(usize) (publisher_confirms)");
+        println!("{}", HELP_PHRASE);
         return;
     }
 
-    if args.len() >= 6 && args[6] == "publisher_confirms" {
-        publisher_confirms = true;
+    if args.len() >= ARG_COUNT + 1 && (args[PUB_CONF_ARG] == "publisher_confirms" ||  args[PUB_CONF_ARG] == "true") {
+            publisher_confirms = true;
     }
 
     if args[1] == "--help" {
-        println!("usage: host user pass queue_name message_len(usize) (publisher_confirms)");
+        println!("{}", HELP_PHRASE);
         return;
     }
     else {
-        let host = &args[1];
-        let user = &args[2];
-        let pass = &args[3];
-        let queue_name = &args[4];
-        let message_len = args[5].parse::<usize>().unwrap();
-        goRabbit(host, user, pass, queue_name, message_len, publisher_confirms);
+        let host = &args[HOST_ARG];
+        let user = &args[USER_ARG];
+        let pass = &args[PASS_ARG];
+        let queue_name = &args[QUEUE_ARG];
+        let message_len = args[LENGTH_ARG].parse::<usize>().unwrap();
+        let go_rabbit = || -> Result<()> {
+            goRabbit(host, user, pass, queue_name, message_len, publisher_confirms)
+        };
+        
+        if let Err(_err) = go_rabbit() {
+            println!("{}", _err);
+        }
     }
 }
 
 fn goRabbit(host: &str, user: &str, pass: &str, queue_name: &str, message_len: usize, publisher_confirms: bool) -> Result<()> {
-    let mut connection_string = &format!("amqp://{}:{}@{}:5672", user, pass, host) as &str; //"amqp://:{pass}@{host}:5672"
+    let mut connection_string = &format!("amqp://{}:{}@{}:5672", user, pass, host) as &str;
     println!("{}", connection_string);
 
     let mut connection = Connection::insecure_open(connection_string)?;
@@ -38,9 +53,7 @@ fn goRabbit(host: &str, user: &str, pass: &str, queue_name: &str, message_len: u
     let exchange = Exchange::direct(&channel);
 
     let message = getMessage(message_len);
-
-    //exchange.publish(Publish::new(&message[0..message.len() - 1], queue_name))?;
-
+    
     if publisher_confirms {
         println!("publisher confirms enabled");
         channel.enable_publisher_confirms();
