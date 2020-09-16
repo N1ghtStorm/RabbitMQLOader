5c60922c36599c7188d888305b781666622c3756
N1ghtStorm <bushuevdv@mail.ru>
2020-09-16 12:35:50 +0300

    переделал функцию

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,17 @@ fn main() -> Result<()> {
     let mut connection = Connection::insecure_open("amqp://guest:guest@localhost:5672")?;
     let channel = connection.open_channel(None)?;
     let exchange = Exchange::direct(&channel);
+
     let message = getMessage(8);
 
     loop {
-        exchange.publish(Publish::new(message, "hello"))?;
+      exchange.publish(Publish::new(&message[0..message.len() - 1], "hello"))?;
     }
     
     connection.close()
 }
 
-fn getMessage(message_size: usize) -> &[u8] {
-    let array: [u8; message_size] = [0, message_size];
-    return &array;
+fn getMessage(message_size: usize) -> Vec<u8> {
+    let mut vector: Vec<u8> = vec![0; message_size];
+    vector
 }
